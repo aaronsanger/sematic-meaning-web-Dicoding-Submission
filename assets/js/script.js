f836dbf90006c893d45a7d93646b7940e6338e28
Aaron Sanger <39895721+aaronsanger@users.noreply.github.com>
2020-06-17 16:04:10 +0700

    Submited to dicoding
    Waiting for review

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+console.log(
+  "Selamat Datang di Website Desa Sumogawe. Disclaimer: Terinspirasi dari salah satu tugas matakuliah."
+);
+alert(
+  "Selamat Datang di Website Desa Sumogawe. Disclaimer: Tema terinspirasi dari salah satu tugas matakuliah."
+);
